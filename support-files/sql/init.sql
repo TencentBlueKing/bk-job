6dfbeffa2f63ee49db7da372adecfdc1aeff8a7e
llitfkitfk <llitfkitfk@gmail.com>
2021-07-06 10:40:40 +0800

    feat: add mysql for docker development with initial sql files #36

---
+++ support-files/sql/init.sql
@@ -0,0 +1,60 @@
+source /docker-entrypoint-initdb.d/job-analysis/0001_job_analysis_20210107-1000_V3.2.1.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-analysis/0002_job_analysis_20210114-1000_V3.2.1.1_mysql.sql
+source /docker-entrypoint-initdb.d/job-analysis/0003_job_analysis_20210114-2000_V3.2.1.1_mysql.sql
+source /docker-entrypoint-initdb.d/job-analysis/0004_job_analysis_20210301-2000_V3.2.6.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-analysis/0005_job_analysis_20210508-2000_V3.3.0.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-analysis/0006_job_analysis_20210604-2000_V3.3.3.0_mysql.sql
+
+source /docker-entrypoint-initdb.d/job-backup/0001_job_backup_20200801-1000_V3.0.7_mysql.sql
+source /docker-entrypoint-initdb.d/job-backup/0002_job_backup_20201001-1000_V3.1.2_mysql.sql
+source /docker-entrypoint-initdb.d/job-backup/0003_job_backup_20201024-1000_V3.1.2.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-backup/0004_job_backup_20210516-1000_V3.3.0.1_mysql.sql
+
+source /docker-entrypoint-initdb.d/job-crontab/0001_job_crontab_20200222-1000_V3.0.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-crontab/0002_job_crontab_20201101-1000_V3.1.3_mysql.sql
+
+source /docker-entrypoint-initdb.d/job-execute/0001_job_execute_20200222-1000_V3.0.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0002_job_execute_20200522-1000_V3.0.3_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0003_job_execute_20200717-1000_V3.0.5_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0004_job_execute_20200723-1000_V3.0.6_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0005_job_execute_20200810-1000_V3.0.7_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0006_job_execute_20200908-1000_V3.0.10.1_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0007_job_execute_20201108-1000_V3.1.3_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0008_job_execute_20201207-1000_V3.1.5_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0009_job_execute_20201214-1000_V3.2.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0010_job_execute_20210113-1000_V3.2.1_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0011_job_execute_20210119-1000_V3.2.2_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0012_job_execute_20210429-1000_V3.3.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0013_job_execute_20210527-1000_V3.3.1.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-execute/0014_job_execute_20210626-1000_V3.3.3.0_mysql.sql
+
+source /docker-entrypoint-initdb.d/job-file-gateway/0001_job_file_gateway_20201214-1000_V3.2.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-file-gateway/0002_job_file_gateway_20210123-1000_V3.2.3_mysql.sql
+source /docker-entrypoint-initdb.d/job-file-gateway/0003_job_file_gateway_20210312-1000_V3.2.7_mysql.sql
+
+source /docker-entrypoint-initdb.d/job-manage/0001_job_manage_20200222-1000_V3.0.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0002_job_manage_20200522-1000_V3.0.3_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0003_job_manage_20200605-1000_V3.0.3.4_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0004_job_manage_20200710-1000_V3.0.6.1_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0005_job_manage_20200723-1000_V3.0.6.2_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0006_job_manage_20200723-1000_V3.0.6.6_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0007_job_manage_20200810-1000_V3.0.7.4_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0008_job_manage_20200824-1000_V3.0.8.3_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0009_job_manage_20200915-1000_V3.1.0.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0010_job_manage_20200925-1000_V3.1.1.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0011_job_manage_20201021-1000_V3.1.2.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0012_job_manage_20201021-1000_V3.2.0.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0012_job_manage_20201117-1000_V3.1.3.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0013_job_manage_20201207-1000_V3.1.4.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0013_job_manage_20210120-1000_V3.1.4.3_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0013_job_manage_20210205-1000_V3.1.4.4_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0014_job_manage_20201220-1000_V3.2.1.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0015_job_manage_20210128-1000_V3.2.4.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0016_job_manage_20210129-1000_V3.2.5.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0017_job_manage_20210423-1000_V3.2.8.2_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0018_job_manage_20210427-1000_V3.3.0.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0019_job_manage_20210430-1000_V3.3.1.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0020_job_manage_20210515-1000_V3.3.1.0_mysql.sql
+source /docker-entrypoint-initdb.d/job-manage/0021_job_manage_20210609-1000_V3.3.4.0_mysql.sql
+
+source /docker-entrypoint-initdb.d/job-ticket/0001_job_ticket_20201214-1000_V3.2.0_mysql.sql
