0490c1b40e0426a0d5ff2e4aea478530f8831168
jsonwan <jsonwan@tencent.com>
2021-08-16 21:19:19 +0800

    feature: job支持容器化部署 #83

--- support-files/kubernetes/images/migration/startup.sh
+++ support-files/kubernetes/images/migration/startup.sh
@@ -15,6 +15,5 @@ mysql --version
 ALL_SQL=($(echo ./sql/*/*.sql))
 echo "ALL_SQL=$ALL_SQL"
 for sql in "${ALL_SQL[@]}"; do
-  echo "mysql -h $BK_JOB_MYSQL_HOST -P $BK_JOB_MYSQL_PORT -u root -p $BK_JOB_MYSQL_ROOT_PASSWORD < $sql"
-  mysql -h $BK_JOB_MYSQL_HOST -P $BK_JOB_MYSQL_PORT -u root -p $BK_JOB_MYSQL_ROOT_PASSWORD < $sql
+  mysql -h $BK_JOB_MYSQL_HOST -P $BK_JOB_MYSQL_PORT -uroot -p$BK_JOB_MYSQL_ROOT_PASSWORD < $sql
 done
