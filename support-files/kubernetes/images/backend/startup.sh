df63c2f2506fda9308743d38fcd96ddba4168e4f
jsonwan <jsonwan@tencent.com>
2021-08-24 12:26:56 +0800

    feature: job支持容器化部署 #83

--- support-files/kubernetes/images/backend/startup.sh
+++ support-files/kubernetes/images/backend/startup.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
-mkdir -p $BK_JOB_LOGS_DIR
-chmod 777 $BK_JOB_LOGS_DIR
+mkdir -p $BK_JOB_LOG_BASE_DIR
+chmod 777 $BK_JOB_LOG_BASE_DIR
+mkdir -p $BK_JOB_LOG_DIR
+chmod 777 $BK_JOB_LOG_DIR
 
 java -server \
      -Dfile.encoding=UTF-8 \
-     -Xloggc:$BK_JOB_LOGS_DIR/gc.log \
+     -Djob.log.dir=$BK_JOB_LOG_BASE_DIR
+     -Xloggc:$BK_JOB_LOG_DIR/gc.log \
      -XX:+UseGCLogFileRotation \
      -XX:NumberOfGCLogFiles=12 \
      -XX:GCLogFileSize=1G \
@@ -14,7 +17,7 @@ java -server \
      -XX:+PrintGCDateStamps \
      -XX:+HeapDumpOnOutOfMemoryError \
      -XX:HeapDumpPath=heap.hprof \
-     -XX:ErrorFile=$BK_JOB_LOGS_DIR/error_sys.log \
+     -XX:ErrorFile=$BK_JOB_LOG_DIR/error_sys.log \
      -Dspring.profiles.active=$BK_JOB_PROFILE \
      $BK_JOB_JVM_OPTION \
      -jar /data/job/$BK_JOB_JAR
