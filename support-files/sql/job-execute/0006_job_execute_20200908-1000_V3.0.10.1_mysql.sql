146212ebb10ad940dfe103804f71ee5f63610311
tyleryuwang <tyleryuwang@tencent.com>
2021-08-26 09:06:39 +0800

    feature: job支持容器化部署 #83

--- support-files/sql/job-execute/0006_job_execute_20200908-1000_V3.0.10.1_mysql.sql
+++ support-files/sql/job-execute/0006_job_execute_20200908-1000_V3.0.10.1_mysql.sql
@@ -1,5 +1,41 @@
-SET NAMES utf8mb4;
-USE job_execute;
+use job_execute;
 
-ALTER TABLE step_instance_script ADD COLUMN script_id VARCHAR(32) NULL COMMENT '脚本Id';
-ALTER TABLE step_instance_script ADD COLUMN script_version_id BIGINT(20) NULL COMMENT '脚本版本Id';
+SET NAMES utf8mb4;
+
+DROP PROCEDURE IF EXISTS job_schema_update;
+
+DELIMITER <JOB_UBF>
+
+CREATE PROCEDURE job_schema_update()
+BEGIN
+
+    DECLARE db VARCHAR(100);
+    SET AUTOCOMMIT = 0;
+    SELECT DATABASE() INTO db;
+        
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.columns
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'step_instance_script'
+                    AND COLUMN_NAME = 'script_id') THEN
+        ALTER TABLE step_instance_script ADD COLUMN script_id VARCHAR(32) NULL;
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.columns
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'step_instance_script'
+                    AND COLUMN_NAME = 'script_version_id') THEN
+        ALTER TABLE step_instance_script ADD COLUMN script_version_id BIGINT(20) NULL;
+    END IF;
+	
+    COMMIT;
+END <JOB_UBF>
+DELIMITER ;
+COMMIT;
+CALL job_schema_update();
+
+DROP PROCEDURE IF EXISTS job_schema_update;
+
+
+
