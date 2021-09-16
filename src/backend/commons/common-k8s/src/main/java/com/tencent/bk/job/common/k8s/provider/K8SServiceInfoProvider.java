7cdfc394f71ef2190665291053b32d21bce4bc83
jsonwan <jsonwan@tencent.com>
2021-09-16 12:03:05 +0800

    feature: job支持容器化部署 #83
    服务状态K8s pod状态获取实现

--- src/backend/commons/common-k8s/src/main/java/com/tencent/bk/job/common/k8s/provider/K8SServiceInfoProvider.java
+++ src/backend/commons/common-k8s/src/main/java/com/tencent/bk/job/common/k8s/provider/K8SServiceInfoProvider.java
@@ -27,10 +27,22 @@ package com.tencent.bk.job.common.k8s.provider;
 import com.tencent.bk.job.common.discovery.ServiceInfoProvider;
 import com.tencent.bk.job.common.discovery.model.ServiceInstanceInfoDTO;
 import com.tencent.bk.job.common.util.json.JsonUtils;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.openapi.models.V1PodStatus;
+import io.kubernetes.client.util.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.kubernetes.commons.discovery.KubernetesServiceInstance;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,12 +51,42 @@ import java.util.stream.Collectors;
 @Slf4j
 public class K8SServiceInfoProvider implements ServiceInfoProvider {
 
+    public final String KEY_HELM_NAMESPACE = "meta.helm.sh/release-namespace";
     public final String KEY_JOB_MS_VERSION = "bk.job.image/tag";
     private final DiscoveryClient discoveryClient;
 
     public K8SServiceInfoProvider(DiscoveryClient discoveryClient) {
         this.discoveryClient = discoveryClient;
         log.debug("K8sServiceInfoServiceImpl inited");
+    }
+
+    private String getNameSpace(ServiceInstance serviceInstance) {
+        KubernetesServiceInstance k8sServiceInstance = (KubernetesServiceInstance) serviceInstance;
+        String namespace = k8sServiceInstance.getNamespace();
+        if (StringUtils.isNotBlank(namespace)) return namespace;
+        return serviceInstance.getMetadata().getOrDefault(KEY_HELM_NAMESPACE, "default");
+    }
+
+    private V1Pod findPodByUid(V1PodList podList, String uid) {
+        if (podList == null) return null;
+        for (V1Pod pod : podList.getItems()) {
+            V1ObjectMeta metaData = pod.getMetadata();
+            if (metaData != null && uid.equals(metaData.getUid())) {
+                return pod;
+            }
+        }
+        return null;
+    }
+
+    private Byte convertPodStatus(V1PodStatus podStatus) {
+        String phase = podStatus.getPhase();
+        if (StringUtils.isNotBlank(phase)) {
+            if (phase.equals("Running"))
+                return (byte) 1;
+            else
+                return (byte) 0;
+        }
+        return (byte) -1;
     }
 
     private void fillVersionAndStatus(
@@ -58,9 +100,34 @@ public class K8SServiceInfoProvider implements ServiceInfoProvider {
         }
         String version = metaData.getOrDefault(KEY_JOB_MS_VERSION, "Unknown");
         serviceInstanceInfoDTO.setVersion(version);
-        // TODO:通过K8s API查询POD状态
-        serviceInstanceInfoDTO.setStatusCode((byte) 1);
-        serviceInstanceInfoDTO.setStatusMessage("");
+        String namespace = getNameSpace(serviceInstance);
+        log.debug("namespace={},version={}", namespace, version);
+        V1PodList podList;
+        try {
+            ApiClient client = Config.defaultClient();
+            Configuration.setDefaultApiClient(client);
+
+            CoreV1Api api = new CoreV1Api();
+            podList = api.listNamespacedPod(
+                namespace, null, null, null,
+                null, null, null, null,
+                null, null, null);
+            if (log.isDebugEnabled()) {
+                log.debug("podList={}", JsonUtils.toJson(podList));
+            }
+        } catch (ApiException | IOException e) {
+            log.error("Fail to get pod info from k8s API", e);
+            return;
+        }
+        V1Pod pod = findPodByUid(podList, serviceInstance.getInstanceId());
+        if (pod.getMetadata() != null) {
+            serviceInstanceInfoDTO.setName(pod.getMetadata().getName());
+        }
+        V1PodStatus podStatus = pod.getStatus();
+        if (podStatus != null) {
+            serviceInstanceInfoDTO.setStatusCode(convertPodStatus(podStatus));
+            serviceInstanceInfoDTO.setStatusMessage(podStatus.getReason());
+        }
     }
 
     @Override
