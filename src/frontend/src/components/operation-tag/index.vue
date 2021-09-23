47f886bd95e7cf643ab20ce82038594f871eeafa
hLinx <327159425@qq.com>
2021-09-23 15:51:25 +0800

    feature: 标签管理功能 #178

--- src/frontend/src/components/operation-tag/index.vue
+++ src/frontend/src/components/operation-tag/index.vue
@@ -118,12 +118,15 @@
                     },
                 ],
             };
-            // props.data 有更新时同步最新的值
-            watch(() => props.data, (data) => {
+            // value 有更新时显示最新的值
+            watch(() => props.value, (value) => {
+                if (!value) {
+                    return;
+                }
                 const {
                     name,
                     description,
-                } = data;
+                } = props.data;
                 state.formData = {
                     name,
                     description,
