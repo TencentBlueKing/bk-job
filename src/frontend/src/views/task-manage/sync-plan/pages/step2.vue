375a85d3794c08fd4aad99957734b02a719df065
hLinx <327159425@qq.com>
2021-09-26 17:42:08 +0800

    fix: 脚本编辑态时点击调试没有将最新内容传递到执行页面 #197

--- src/frontend/src/views/task-manage/sync-plan/pages/step2.vue
+++ src/frontend/src/views/task-manage/sync-plan/pages/step2.vue
@@ -42,11 +42,11 @@
                         <div class="sync-layout">
                             <div class="sync-before block-title">
                                 {{ $t('template.全局变量.label') }}
-                                <span class="global-variable-tips">{{ $t('template.全局变量.label的 “初始值” 不会被同步到执行方案') }}</span>
+                                <span class="global-variable-tips">{{ $t('template.全局变量的 “初始值” 不会被同步到执行方案') }}</span>
                             </div>
                             <div class="sync-after block-title">
                                 {{ $t('template.全局变量.label') }}
-                                <span class="global-variable-tips">{{ $t('template.全局变量.label的 “初始值” 不会被同步到执行方案') }}</span>
+                                <span class="global-variable-tips">{{ $t('template.全局变量的 “初始值” 不会被同步到执行方案') }}</span>
                             </div>
                         </div>
                         <template v-for="index in templateVariableList.length">
