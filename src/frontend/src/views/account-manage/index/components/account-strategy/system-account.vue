2bb48c593fb6da927eb72b383760a4a12b365a4f
hLinx <327159425@qq.com>
2021-10-21 12:01:36 +0800

    perf: 调整账号管理逻辑 #346

--- src/frontend/src/views/account-manage/index/components/account-strategy/system-account.vue
+++ src/frontend/src/views/account-manage/index/components/account-strategy/system-account.vue
@@ -27,7 +27,9 @@
 
 <template functional>
     <div key="system-account">
-        <jb-form-item :label="parent.$t('account.类型')" required>
+        <jb-form-item
+            :label="parent.$t('account.类型')"
+            required>
             <div class="radio-button-group-wraper">
                 <bk-radio-group
                     :value="props.formData.type"
@@ -47,14 +49,20 @@
                 </bk-radio-group>
             </div>
         </jb-form-item>
-        <jb-form-item :label="parent.$t('account.名称')" required property="account">
+        <jb-form-item
+            :label="parent.$t('account.名称')"
+            required
+            property="account">
             <jb-input
                 :placeholder="props.namePlaceholder"
                 :readonly="props.isEdit"
                 :value="props.formData.account"
                 @change="value => props.change('account', value)" />
         </jb-form-item>
-        <jb-form-item :label="parent.$t('account.别名')" required property="alias">
+        <jb-form-item
+            :label="parent.$t('account.别名')"
+            required
+            property="alias">
             <jb-input
                 :placeholder="parent.$t('account.在出现同名账号的情况下，账号的别名显得格外重要...')"
                 :value="props.formData.alias"
@@ -63,14 +71,22 @@
         </jb-form-item>
         <!-- Linux系统账号不需要密码 -->
         <template v-if="props.formData.type !== 1">
-            <jb-form-item key="systemPassword" :label="parent.$t('account.密码')" required property="password">
+            <jb-form-item
+                key="systemPassword"
+                :label="parent.$t('account.密码')"
+                required
+                property="password">
                 <bk-input
                     :placeholder="parent.$t('account.输入密码')"
                     type="password"
                     :value="props.formData.password"
                     @change="value => props.change('password', value)" />
             </jb-form-item>
-            <jb-form-item key="systemRepassword" :label="parent.$t('account.确认密码')" required property="rePassword">
+            <jb-form-item
+                key="systemRepassword"
+                :label="parent.$t('account.确认密码')"
+                required
+                property="rePassword">
                 <bk-input
                     :placeholder="parent.$t('account.输入确认密码')"
                     type="password"
