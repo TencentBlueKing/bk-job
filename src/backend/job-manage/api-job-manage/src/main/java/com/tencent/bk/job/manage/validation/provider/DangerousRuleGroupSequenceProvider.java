3114a1432c820d30a1611544c926f0cf7616eb49
smarttill <smarttill@canway.net>
2022-01-18 11:21:55 +0800

    bugfix: 高危脚本规则新建编辑操作，后端没有校验参数 #616

--- src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/validation/provider/DangerousRuleGroupSequenceProvider.java
+++ src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/validation/provider/DangerousRuleGroupSequenceProvider.java
@@ -33,6 +33,9 @@ import com.tencent.bk.job.common.validation.Create;
 import com.tencent.bk.job.common.validation.Update;
 import com.tencent.bk.job.manage.model.web.request.globalsetting.AddOrUpdateDangerousRuleReq;
 
+/**
+ * id属性值如果不为空并且不为-1，执行更新组的校验逻辑，否则执行新增组的校验逻辑
+ */
 public class DangerousRuleGroupSequenceProvider implements DefaultGroupSequenceProvider<AddOrUpdateDangerousRuleReq> {
 
     @Override
