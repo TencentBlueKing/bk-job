c8f6c7c6a79f7a39fc11283170c34a5acc733a60
wangyu096 <wangyu096@163.com>
2022-01-20 11:06:22 +0800

    feature: 原子操作能力支持滚动执行 #446

--- src/backend/job-execute/api-job-execute/src/main/java/com/tencent/bk/job/execute/model/web/vo/ExecuteStepVO.java
+++ src/backend/job-execute/api-job-execute/src/main/java/com/tencent/bk/job/execute/model/web/vo/ExecuteStepVO.java
@@ -51,6 +51,6 @@ public class ExecuteStepVO {
     private ExecuteApprovalStepVO approvalStepInfo;
 
     @ApiModelProperty(value = "滚动配置")
-    private FastJobRollingConfigVO rollingExecutionConfig;
+    private FastJobRollingConfigVO rollingConfig;
 
 }
