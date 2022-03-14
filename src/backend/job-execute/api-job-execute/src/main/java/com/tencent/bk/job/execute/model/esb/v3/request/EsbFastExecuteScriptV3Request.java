78adb68077c44cca3bdcbcb7e04a001cf3a1651d
wangyu096 <wangyu096@163.com>
2022-03-14 16:39:29 +0800

    feature: 业务集改造-ESB API 支持 #709

--- src/backend/job-execute/api-job-execute/src/main/java/com/tencent/bk/job/execute/model/esb/v3/request/EsbFastExecuteScriptV3Request.java
+++ src/backend/job-execute/api-job-execute/src/main/java/com/tencent/bk/job/execute/model/esb/v3/request/EsbFastExecuteScriptV3Request.java
@@ -26,7 +26,7 @@ package com.tencent.bk.job.execute.model.esb.v3.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tencent.bk.job.common.constant.JobConstants;
-import com.tencent.bk.job.common.esb.model.EsbReq;
+import com.tencent.bk.job.common.esb.model.EsbAppScopeReq;
 import com.tencent.bk.job.common.esb.model.job.EsbIpDTO;
 import com.tencent.bk.job.common.esb.model.job.v3.EsbServerV3DTO;
 import lombok.Getter;
@@ -41,7 +41,7 @@ import java.util.List;
  */
 @Getter
 @Setter
-public class EsbFastExecuteScriptV3Request extends EsbReq {
+public class EsbFastExecuteScriptV3Request extends EsbAppScopeReq {
     /**
      * 业务ID
      */
