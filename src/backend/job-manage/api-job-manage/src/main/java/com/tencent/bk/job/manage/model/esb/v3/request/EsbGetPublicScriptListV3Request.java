9305d5db0ea1baf99eec8d5b690644592dd94500
wangyu096 <wangyu096@163.com>
2022-04-09 20:15:30 +0800

    feature: 业务集改造-问题修复、代码重构 #728
    修复公共脚本API返回500错误的问题

--- src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/model/esb/v3/request/EsbGetPublicScriptListV3Request.java
+++ src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/model/esb/v3/request/EsbGetPublicScriptListV3Request.java
@@ -44,14 +44,4 @@ public class EsbGetPublicScriptListV3Request extends EsbJobReq {
      */
     @JsonProperty("script_language")
     private Integer scriptLanguage;
-
-    /**
-     * 起始位置
-     */
-    private Integer start;
-
-    /**
-     * 起始位置
-     */
-    private Integer length;
 }
