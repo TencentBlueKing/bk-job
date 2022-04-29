ca5e94c30a4528429caca86d2ac20b6fea239c8d
wangyu096 <wangyu096@163.com>
2022-04-29 11:28:38 +0800

    feature: 业务集改造-问题修复、代码重构 #728

--- support-files/sql/job-manage/0025_job_manage_20220302-1000_V3.5.0.0_mysql.sql
+++ support-files/sql/job-manage/0025_job_manage_20220302-1000_V3.5.0.0_mysql.sql
@@ -1,18 +1,6 @@
-USE job_manage;
+  -- Update `application` schema
+  ALTER TABLE `application` MODIFY `app_id` BIGINT(20) UNSIGNED AUTO_INCREMENT;
 
-SET NAMES utf8mb4;
-
-DROP PROCEDURE IF EXISTS job_schema_update;
-
-DELIMITER <JOB_UBF>
-
-CREATE PROCEDURE job_schema_update()
-BEGIN
-
-  DECLARE db VARCHAR(100);
-  SET AUTOCOMMIT = 0;
-  SELECT DATABASE() INTO db;
-	
   IF NOT EXISTS(SELECT 1
                 FROM information_schema.columns
                 WHERE TABLE_SCHEMA = db
@@ -36,21 +24,7 @@ BEGIN
                     AND COLUMN_NAME = 'attrs') THEN
     ALTER TABLE application ADD COLUMN attrs text;
   END IF;
-    
-  UPDATE application SET bk_scope_type = 'biz' WHERE app_type = 1 AND (bk_scope_type IS NULL OR bk_scope_type = '');
-  UPDATE application SET bk_scope_type = 'biz_set' WHERE app_type in (2,3) AND (bk_scope_type IS NULL OR bk_scope_type = '');
-  UPDATE application SET bk_scope_id = app_id WHERE bk_scope_id is NULL OR bk_scope_id = '';
-	
-  IF NOT EXISTS(SELECT 1
-                  FROM information_schema.statistics
-                  WHERE TABLE_SCHEMA = db
-                    AND TABLE_NAME = 'application'
-                    AND INDEX_NAME = 'uk_scope_id_scope_type') THEN
-    ALTER TABLE application ADD UNIQUE INDEX uk_scope_id_scope_type(`bk_scope_id`,`bk_scope_type`);
-  END IF;
-
-  ALTER TABLE `application` MODIFY `app_id` BIGINT(20) UNSIGNED AUTO_INCREMENT;
-	
+  
   IF NOT EXISTS(SELECT 1
                   FROM information_schema.columns
                   WHERE TABLE_SCHEMA = db
@@ -66,8 +40,8 @@ BEGIN
                     AND INDEX_NAME = 'app_type') THEN
     ALTER TABLE application DROP INDEX `app_type`;
   END IF;
-	
-	
+  
+  -- Update `host` schema
   IF NOT EXISTS(SELECT 1
                   FROM information_schema.columns
                   WHERE TABLE_SCHEMA = db
@@ -76,7 +50,14 @@ BEGIN
     ALTER TABLE host ADD COLUMN cloud_ip VARCHAR(65) NOT NULL DEFAULT '';
   END IF;
 
-	UPDATE host SET cloud_ip = concat(cloud_area_id,':',ip) WHERE cloud_ip='';
+  -- Update data
+  UPDATE application SET bk_scope_type = 'biz' WHERE app_type = 1 AND (bk_scope_type IS NULL OR bk_scope_type = '');
+  UPDATE application SET bk_scope_type = 'biz_set' WHERE app_type in (2,3) AND (bk_scope_type IS NULL OR bk_scope_type = '');
+  UPDATE application SET bk_scope_id = app_id WHERE bk_scope_id is NULL OR bk_scope_id = '';
+  UPDATE host SET cloud_ip = concat(cloud_area_id,':',ip) WHERE cloud_ip='';
+  -- 结束事务，提交表数据变更
+  COMMIT;
+  
 
   IF NOT EXISTS(SELECT 1
                   FROM information_schema.statistics
@@ -85,7 +66,15 @@ BEGIN
                     AND INDEX_NAME = 'idx_cloud_ip') THEN
       ALTER TABLE host ADD INDEX idx_cloud_ip(`cloud_ip`);
   END IF;
-	
+  
+  IF NOT EXISTS(SELECT 1
+                  FROM information_schema.statistics
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'application'
+                    AND INDEX_NAME = 'uk_scope_id_scope_type') THEN
+    ALTER TABLE application ADD UNIQUE INDEX uk_scope_id_scope_type(`bk_scope_id`,`bk_scope_type`);
+  END IF;
+  
   COMMIT;
 END <JOB_UBF>
 DELIMITER ;
