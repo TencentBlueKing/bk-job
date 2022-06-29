e01a4953250b6c075ca44eccfd636aa136eb8ddf
jsonwan <jsonwan@tencent.com>
2022-06-29 18:13:04 +0800

    perf: IPv6-优化执行类接口调用时主机不存在的错误提示 #1078
    综合给出hostId与cloudIp信息

--- src/backend/job-execute/service-job-execute/src/main/java/com/tencent/bk/job/execute/service/impl/TaskExecuteServiceImpl.java
+++ src/backend/job-execute/service-job-execute/src/main/java/com/tencent/bk/job/execute/service/impl/TaskExecuteServiceImpl.java
@@ -824,10 +824,11 @@ public class TaskExecuteServiceImpl implements TaskExecuteService {
     }
 
     private void throwHostInvalidException(Collection<HostDTO> unavailableHosts, long appId) {
-        String ipListStr = StringUtils.join(unavailableHosts.stream().map(HostDTO::getIp).collect(Collectors.toList()),
-            ",");
-        log.warn("The following hosts are not registered, appId:{}, ips={}", appId, ipListStr);
-        throw new FailedPreconditionException(ErrorCode.HOST_NOT_EXIST, new Object[]{ipListStr});
+        String unavailableHostListStr = unavailableHosts.parallelStream()
+            .map(HostDTO::getHostIdAndIpDescription)
+            .collect(Collectors.joining());
+        log.warn("The following hosts are not registered, appId:{}, hosts={}", appId, unavailableHostListStr);
+        throw new FailedPreconditionException(ErrorCode.HOST_NOT_EXIST, new Object[]{unavailableHostListStr});
     }
 
     private void checkStepInstanceConstraint(TaskInstanceDTO taskInstance, List<StepInstanceDTO> stepInstanceList) {
