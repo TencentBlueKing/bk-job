ae9a4fe70addc75f3f85ee7b70a1a3c93ea9b5e8
jsonwan <jsonwan@tencent.com>
2022-09-01 18:30:19 +0800

    feature: IP选择器后台接口IPv6改造 #1181
    处理gateway负载均衡路由报错问题

--- src/backend/job-gateway/src/main/java/com/tencent/bk/job/gateway/config/Ipv6ReactiveLoadBalancerClientConfiguration.java
+++ src/backend/job-gateway/src/main/java/com/tencent/bk/job/gateway/config/Ipv6ReactiveLoadBalancerClientConfiguration.java
@@ -24,15 +24,24 @@
 
 package com.tencent.bk.job.gateway.config;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerUriTools;
 import org.springframework.cloud.gateway.config.GatewayLoadBalancerProperties;
 import org.springframework.cloud.gateway.filter.ReactiveLoadBalancerClientFilter;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
+
+/**
+ * 用于修复spring-cloud-gateway在IPv6环境下的负载均衡路由报错问题
+ */
 @Configuration
-public class JobReactiveLoadBalancerClientConfiguration {
+public class Ipv6ReactiveLoadBalancerClientConfiguration {
 
     @Bean
     public ReactiveLoadBalancerClientFilter ipv6GatewayLoadBalancerClientFilter(
@@ -43,4 +52,39 @@ public class JobReactiveLoadBalancerClientConfiguration {
         return new Ipv6ReactiveLoadBalancerClientFilter(clientFactory, properties, loadBalancerProperties);
     }
 
+    static class Ipv6ReactiveLoadBalancerClientFilter extends ReactiveLoadBalancerClientFilter {
+
+        public Ipv6ReactiveLoadBalancerClientFilter(
+            LoadBalancerClientFactory clientFactory,
+            GatewayLoadBalancerProperties properties,
+            LoadBalancerProperties loadBalancerProperties) {
+            super(clientFactory, properties, loadBalancerProperties);
+        }
+
+        @Override
+        protected URI reconstructURI(ServiceInstance serviceInstance, URI original) {
+            return LoadBalancerUriTools.reconstructURI(buildHostAdjustedInstance(serviceInstance), original);
+        }
+
+        private ServiceInstance buildHostAdjustedInstance(ServiceInstance originServiceInstance) {
+            DefaultServiceInstance serviceInstance = new DefaultServiceInstance(
+                originServiceInstance.getInstanceId(),
+                originServiceInstance.getServiceId(),
+                originServiceInstance.getHost(),
+                originServiceInstance.getPort(),
+                originServiceInstance.isSecure(),
+                originServiceInstance.getMetadata()
+            );
+            processIpv6Host(serviceInstance);
+            return serviceInstance;
+        }
+
+        private void processIpv6Host(DefaultServiceInstance serviceInstance) {
+            String host = serviceInstance.getHost();
+            if (StringUtils.isNotBlank(host) && host.contains(":") && !host.startsWith("[")) {
+                serviceInstance.setHost("[" + host + "]");
+            }
+        }
+
+    }
 }
