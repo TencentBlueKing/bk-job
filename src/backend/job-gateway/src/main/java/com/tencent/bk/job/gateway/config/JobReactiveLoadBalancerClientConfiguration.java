ea4b70846722151b33bb446925d613082967e65c
jsonwan <jsonwan@tencent.com>
2022-09-01 18:13:47 +0800

    feature: IP选择器后台接口IPv6改造 #1181
    处理gateway负载均衡路由报错问题

--- src/backend/job-gateway/src/main/java/com/tencent/bk/job/gateway/config/JobReactiveLoadBalancerClientConfiguration.java
+++ src/backend/job-gateway/src/main/java/com/tencent/bk/job/gateway/config/JobReactiveLoadBalancerClientConfiguration.java
@@ -24,26 +24,17 @@
 
 package com.tencent.bk.job.gateway.config;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerProperties;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactiveLoadBalancer;
 import org.springframework.cloud.gateway.config.GatewayLoadBalancerProperties;
 import org.springframework.cloud.gateway.config.conditional.ConditionalOnEnabledGlobalFilter;
 import org.springframework.cloud.gateway.filter.ReactiveLoadBalancerClientFilter;
-import org.springframework.cloud.loadbalancer.config.LoadBalancerAutoConfiguration;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.DispatcherHandler;
 
-@Configuration(proxyBeanMethods = false)
-@ConditionalOnClass({ReactiveLoadBalancer.class, LoadBalancerAutoConfiguration.class, DispatcherHandler.class})
-@AutoConfigureAfter(LoadBalancerAutoConfiguration.class)
-@EnableConfigurationProperties(GatewayLoadBalancerProperties.class)
-public class JobReactiveLoadBalancerClientAutoConfiguration {
+@Configuration
+public class JobReactiveLoadBalancerClientConfiguration {
 
     @Bean
     @ConditionalOnBean(LoadBalancerClientFactory.class)
