95db4b3797c8e271d2e197c217c3f2eda84be6d9
jsonwan <jsonwan@tencent.com>
2022-09-14 09:35:50 +0800

    perf: 接入蓝鲸监控APM调用链追踪 #1161
    自定义Otel SpanExporter配置

--- src/backend/commons/common-service/src/main/java/com/tencent/bk/job/common/tracing/OtelConfiguration.java
+++ src/backend/commons/common-service/src/main/java/com/tencent/bk/job/common/tracing/OtelConfiguration.java
@@ -27,17 +27,32 @@ package com.tencent.bk.job.common.tracing;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
+import io.opentelemetry.sdk.trace.SpanLimits;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.sleuth.autoconfig.otel.SpanProcessorProvider;
+import org.springframework.cloud.sleuth.otel.bridge.SpanExporterCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnProperty(value = "spring.sleuth.otel.resource.enabled", matchIfMissing = true)
-class BkDataTokenOtelResourceConfiguration {
+class OtelConfiguration {
+
+    @Value("${spring.sleuth.otel.exporter.enabled:false}")
+    private boolean exporterEnabled;
 
     @Value("${spring.sleuth.otel.resource.bkDataToken:}")
     private String bkDataToken;
@@ -51,5 +66,26 @@ class BkDataTokenOtelResourceConfiguration {
         AttributesBuilder attributes = Attributes.builder();
         attributes.put("bk.data.token", bkDataToken);
         return Resource.create(attributes.build(), ResourceAttributes.SCHEMA_URL);
+    }
+
+    @Bean
+    SdkTracerProvider otelTracerProvider(SpanLimits spanLimits,
+                                         ObjectProvider<List<SpanProcessor>> spanProcessors,
+                                         SpanExporterCustomizer spanExporterCustomizer,
+                                         ObjectProvider<List<SpanExporter>> spanExporters,
+                                         Sampler sampler,
+                                         Resource resource,
+                                         SpanProcessorProvider spanProcessorProvider) {
+        log.debug("exporterEnabled={},bkDataToken={}", exporterEnabled, bkDataToken);
+        SdkTracerProviderBuilder sdkTracerProviderBuilder = SdkTracerProvider.builder().setResource(resource)
+            .setSampler(sampler).setSpanLimits(spanLimits);
+        List<SpanProcessor> processors = spanProcessors.getIfAvailable(ArrayList::new);
+        if (exporterEnabled) {
+            processors.addAll(spanExporters.getIfAvailable(ArrayList::new).stream()
+                .map(e -> spanProcessorProvider.toSpanProcessor(spanExporterCustomizer.customize(e)))
+                .collect(Collectors.toList()));
+        }
+        processors.forEach(sdkTracerProviderBuilder::addSpanProcessor);
+        return sdkTracerProviderBuilder.build();
     }
 }
