3434aa20dbd5fcbb0241e8d95750f8f6ac33d641
jsonwan <jsonwan@tencent.com>
2022-10-26 18:08:04 +0800

    feature: IP选择器后台接口IPv6改造 #1181
    更新无效主机错误提示

--- src/backend/commons/common/src/main/java/com/tencent/bk/job/common/constant/ErrorCode.java
+++ src/backend/commons/common/src/main/java/com/tencent/bk/job/common/constant/ErrorCode.java
@@ -194,7 +194,7 @@ public class ErrorCode {
     // 作业执行 start
     // 主机为空
     public static final int SERVER_EMPTY = 1244001;
-    // 主机[{0}]无效，请检查源或目标主机是否存在于配置平台；另外，主机跨业务需要联系作业平台管理员先将其添加到IP白名单。
+    // 主机[{0}]无效，请检查源或目标主机的IPv4或AgentID字段值是否存在于配置平台；另外，主机需跨业务执行请联系作业平台管理员将其添加到主机白名单。
     public static final int HOST_INVALID = 1244002;
     // 账号不存在
     public static final int ACCOUNT_NOT_EXIST = 1244003;
