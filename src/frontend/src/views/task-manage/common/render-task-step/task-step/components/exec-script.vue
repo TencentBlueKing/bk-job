4f8d23e4c7f8b0925b23d8dfd7e50afc5cfafec2
hLinx <327159425@qq.com>
2022-11-24 14:56:41 +0800

    bugfix: 作业编辑态时脚本内出现“扫描”级别的高危语句无法保存 #1507

--- src/frontend/src/views/task-manage/common/render-task-step/task-step/components/exec-script.vue
+++ src/frontend/src/views/task-manage/common/render-task-step/task-step/components/exec-script.vue
@@ -228,8 +228,8 @@
                 return this.$refs.form.validate()
                     .then(() => true, () => false)
                     .then(validate => scriptErrorConfirm().then(() => {
-                            this.$emit('on-change', result, validate);
-                        }));
+                        this.$emit('on-change', result, validate);
+                    }));
             },
         },
     };
