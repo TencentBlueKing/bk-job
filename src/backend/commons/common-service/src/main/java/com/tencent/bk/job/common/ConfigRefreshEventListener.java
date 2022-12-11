ee4da7b03cfbcfd8b2d32adf771832e480ce93e2
wangyu096 <wangyu096@163.com>
2022-12-11 22:55:47 +0800

    feature: Job对接GSE1.0/2.0支持灰度 #1602

---
+++ src/backend/commons/common-service/src/main/java/com/tencent/bk/job/common/ConfigRefreshEventListener.java
@@ -0,0 +1,22 @@
+package com.tencent.bk.job.common;
+
+import com.tencent.bk.job.common.util.feature.FeatureToggle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ConfigRefreshEventListener {
+    /**
+     * 监听并处理配置更新事件
+     *
+     * @param event 配置更新事件
+     */
+    public void onEvent(EnvironmentChangeEvent event) {
+        log.info("Handle EnvironmentChangeEvent, event: {}", event);
+        log.info("Reload feature toggle start ..");
+        FeatureToggle.getInstance().reload();
+        log.info("Reload feature toggle successfully");
+    }
+}
