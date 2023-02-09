df40088b5bf5f7cdde13dce15ce80aca8681e0e5
wangyu096 <wangyu096@163.com>
2023-02-09 14:34:40 +0800

    perf: 升级spring + springboot + spring cloud 框架版本 #1701

--- src/backend/job-crontab/boot-job-crontab/src/main/java/com/tencent/bk/job/crontab/config/SwaggerConfig.java
+++ src/backend/job-crontab/boot-job-crontab/src/main/java/com/tencent/bk/job/crontab/config/SwaggerConfig.java
@@ -61,36 +61,4 @@ public class SwaggerConfig {
             .paths(PathSelectors.any())
             .build();
     }
-
-//    /**
-//     * 增加如下配置可解决Spring Boot 6.x 与Swagger 3.0.0 不兼容问题
-//     **/
-//    @Bean
-//    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
-//                                                                         ServletEndpointsSupplier servletEndpointsSupplier,
-//                                                                         ControllerEndpointsSupplier controllerEndpointsSupplier,
-//                                                                         EndpointMediaTypes endpointMediaTypes,
-//                                                                         CorsEndpointProperties corsProperties,
-//                                                                         WebEndpointProperties webEndpointProperties,
-//                                                                         Environment environment) {
-//        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
-//        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
-//        allEndpoints.addAll(webEndpoints);
-//        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-//        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-//        String basePath = webEndpointProperties.getBasePath();
-//        EndpointMapping endpointMapping = new EndpointMapping(basePath);
-//        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment,
-//            basePath);
-//        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
-//            corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
-//            shouldRegisterLinksMapping, null);
-//    }
-//
-//    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties,
-//                                               Environment environment,
-//                                               String basePath) {
-//        return webEndpointProperties.getDiscovery().isEnabled()
-//            && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
-//    }
 }
