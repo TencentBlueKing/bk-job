fd98a2ba4156ae690dcacde3eafe2297abbbdbb7
hLinx <327159425@qq.com>
2023-02-24 16:13:35 +0800

    feature: 搜索为空样式规范 #1767

--- src/frontend/src/views/script-manage/version/index.vue
+++ src/frontend/src/views/script-manage/version/index.vue
@@ -258,6 +258,25 @@
                                 :size="tableSize"
                                 @setting-change="handleSettingChange" />
                         </bk-table-column>
+                        <empty
+                            v-if="isSearching"
+                            slot="empty"
+                            type="search">
+                            <div>
+                                <div style="font-size: 14px; color: #63656e;">
+                                    {{ $t('搜索结果为空') }}
+                                </div>
+                                <div style="margin-top: 8px; font-size: 12px; line-height: 16px; color: #979ba5;">
+                                    <span>{{ $t('可以尝试调整关键词') }}</span>
+                                    <span>{{ $t('或') }}</span>
+                                    <bk-button
+                                        text
+                                        @click="handleClearSearch">
+                                        {{ $t('清空搜索条件') }}
+                                    </bk-button>
+                                </div>
+                            </div>
+                        </empty>
                     </bk-table>
                     <template slot="flod">
                         <component
@@ -341,6 +360,7 @@
     import PublicScriptService from '@service/public-script-manage';
     import NotifyService from '@service/notify';
     import ScriptModel from '@model/script/script';
+    import Empty from '@components/empty';
     import JbSearchSelect from '@components/jb-search-select';
     import ListActionLayout from '@components/list-action-layout';
     import JbPopoverConfirm from '@components/jb-popover-confirm';
@@ -366,6 +386,7 @@
     export default {
         name: 'ScriptVersion',
         components: {
+            Empty,
             ListActionLayout,
             JbPopoverConfirm,
             JbSearchSelect,
@@ -383,6 +404,7 @@
                 isLoading: false,
                 isListFlod: false,
                 isShowNewVersion: false,
+                isSearching: false,
                 showDiff: false,
                 dataMemo: [],
                 data: [],
@@ -513,7 +535,7 @@
             this.scriptId = this.$route.params.id;
 
             this.fetchData(true);
-            
+
             this.searchSelect = [
                 {
                     name: I18n.t('script.版本号.colHead'),
@@ -625,7 +647,7 @@
                 const windowHeight = window.innerHeight;
                 this.tableHeight = windowHeight - top - 20;
             },
-            
+
             rowClassName ({ row }) {
                 return row.scriptVersionId === this.selectVersionId ? 'active' : '';
             },
@@ -685,7 +707,14 @@
                     list = list.filter(item => reg.test(item[key]));
                 });
                 this.data = Object.freeze(list);
+                this.isSearching = Object.keys(payload).length > 0;
                 this.handleLayoutFlod();
+            },
+            /**
+             * @desc 清空搜索
+             */
+            handleClearSearch () {
+                this.$refs.searchSelect.reset();
             },
             /**
              * @desc 列表排序
@@ -824,7 +853,7 @@
              */
             handleSync (row) {
                 const routerName = this.isPublicScript ? 'scriptPublicSync' : 'scriptSync';
-                
+
                 this.$router.push({
                     name: routerName,
                     params: {
@@ -965,7 +994,7 @@
             handleEditCancel () {
                 this.displayCom = 'detail';
             },
-            
+
             /**
              * @desc 判断是否可以选中
              * @param {Object} row 脚本数据
