e518c564c4e44dc79aa7091aa4b3cd31026cc54d
liuliaozhong <liuliaozhong@canway.net>
2023-03-27 22:15:05 +0800

    perf: 首页Agent状态分布统计接口性能优化 #745

--- src/backend/job-manage/service-job-manage/src/main/java/com/tencent/bk/job/manage/dao/ApplicationHostDAO.java
+++ src/backend/job-manage/service-job-manage/src/main/java/com/tencent/bk/job/manage/dao/ApplicationHostDAO.java
@@ -29,6 +29,7 @@ import com.tencent.bk.job.common.model.BaseSearchCondition;
 import com.tencent.bk.job.common.model.PageData;
 import com.tencent.bk.job.common.model.dto.ApplicationHostDTO;
 import com.tencent.bk.job.common.model.dto.HostSimpleDTO;
+import com.tencent.bk.job.common.model.dto.HostStatusNumStatisticsDTO;
 
 import java.util.Collection;
 import java.util.List;
@@ -173,4 +174,11 @@ public interface ApplicationHostDAO {
      * @return 成功更新的条数
      */
     int batchUpdateHostStatusByHostIds(int status, List<Long> hostIdList);
+
+    /**
+     * 根据业务id统计主机状态数量
+     * @param bizIds 业务id
+     * @return 状态数量
+     */
+    List<HostStatusNumStatisticsDTO> countHostStatusNumByBizIds(List<Long> bizIds);
 }
