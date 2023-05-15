9fdebebd3c6b7f98d648ddc2ca4fd90a1ad15db3
wangyu096 <wangyu096@163.com>
2023-05-15 14:46:37 +0800

    perf: Job支持轻量化部署 #1894

--- src/backend/boot-assemble/src/main/java/com/tencent/bk/job/boot/assemble/JobAssembleBootApplication.java
+++ src/backend/boot-assemble/src/main/java/com/tencent/bk/job/boot/assemble/JobAssembleBootApplication.java
@@ -27,13 +27,12 @@ package com.tencent.bk.job.boot.assemble;
 import com.tencent.bk.job.common.config.FeatureToggleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = "com.tencent.bk.job", exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "com.tencent.bk.job")
 @EnableCaching
 @EnableScheduling
 @EnableConfigurationProperties({FeatureToggleConfig.class})
--- src/backend/job-manage/boot-job-manage/src/main/java/com/tencent/bk/job/manage/JobManageBootApplication.java
+++ src/backend/job-manage/boot-job-manage/src/main/java/com/tencent/bk/job/manage/JobManageBootApplication.java
@@ -27,13 +27,12 @@ package com.tencent.bk.job.manage;
 import com.tencent.bk.job.common.config.FeatureToggleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = "com.tencent.bk.job.manage", exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "com.tencent.bk.job.manage")
 @EnableConfigurationProperties({FeatureToggleConfig.class})
 @EnableCaching
 @EnableFeignClients
