2a69936458e2f5126776a5e662e3f937ddc2787e
jsonwan <jsonwan@tencent.com>
2023-07-19 16:57:38 +0800

    perf: 文件源任务数量较多时DB高负载 #2246
    更新条件字段添加索引

---
+++ support-files/sql/job-execute/0022_job_execute_20230719-1000_V3.7.6_mysql.sql
@@ -0,0 +1,31 @@
+USE job_execute;
+
+SET NAMES utf8mb4;
+
+DROP PROCEDURE IF EXISTS job_schema_update;
+
+DELIMITER <JOB_UBF>
+
+CREATE PROCEDURE job_schema_update()
+BEGIN
+
+  DECLARE db VARCHAR(100);
+  SET AUTOCOMMIT = 0;
+  SELECT DATABASE() INTO db;
+
+  IF NOT EXISTS(SELECT 1
+                FROM information_schema.statistics
+                WHERE TABLE_SCHEMA = db
+                  AND TABLE_NAME = 'file_source_task_log'
+                  AND INDEX_NAME = 'idx_file_source_batch_task_id') THEN
+      ALTER TABLE file_source_task_log ADD INDEX `idx_file_source_batch_task_id` (file_source_batch_task_id);
+  END IF;
+
+  COMMIT;
+END <JOB_UBF>
+DELIMITER ;
+COMMIT;
+
+CALL job_schema_update();
+
+DROP PROCEDURE IF EXISTS job_schema_update;
