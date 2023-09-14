5992135bce7c85bdb2fa004f0c50262155a94812
liuliaozhong <liuliaozhong@canway.net>
2023-09-14 19:20:12 +0800

    fix: 作业模板编辑鉴权时未校验关联的查看权限 #2111

--- src/backend/job-manage/service-job-manage/src/main/java/com/tencent/bk/job/manage/api/web/impl/WebPermissionResourceImpl.java
+++ src/backend/job-manage/service-job-manage/src/main/java/com/tencent/bk/job/manage/api/web/impl/WebPermissionResourceImpl.java
@@ -234,10 +234,16 @@ public class WebPermissionResourceImpl implements WebPermissionResource {
                 );
             case "edit":
                 templateId = Long.parseLong(resourceId);
-                AuthResult authEditResult = templateAuthService.authEditJobTemplate(username, appResourceScope,
-                    templateId);
-                AuthResult authViewResult = templateAuthService.authViewJobTemplate(username, appResourceScope,
-                    templateId);
+                AuthResult authEditResult = templateAuthService.authEditJobTemplate(
+                    username,
+                    appResourceScope,
+                    templateId
+                );
+                AuthResult authViewResult = templateAuthService.authViewJobTemplate(
+                    username,
+                    appResourceScope,
+                    templateId
+                );
                 return Response.buildSuccessResp(
                     webAuthService.toAuthResultVO(
                         isReturnApplyUrl,
