1af9fa0b3bf5db2391b1fb4ebdf78ac2a28cfa69
jsonwan <jsonwan@tencent.com>
2023-09-15 19:39:54 +0800

    fix: file-worker任务状态更新请求无序到达导致第三方源文件偶现分发失败 #2434
    统计子任务完成状态时加锁，防止应用统计结果的过程中数据被修改

--- src/backend/job-file-gateway/service-job-file-gateway/src/main/java/com/tencent/bk/job/file_gateway/dao/filesource/FileSourceTaskDAO.java
+++ src/backend/job-file-gateway/service-job-file-gateway/src/main/java/com/tencent/bk/job/file_gateway/dao/filesource/FileSourceTaskDAO.java
@@ -41,7 +41,7 @@ public interface FileSourceTaskDAO {
 
     FileSourceTaskDTO getFileSourceTaskByIdForUpdate(String id);
 
-    Long countFileSourceTasksByBatchTaskId(String batchTaskId, Byte status);
+    Long countFileSourceTasksByBatchTaskIdForUpdate(String batchTaskId, Byte status);
 
     List<FileSourceTaskDTO> listByBatchTaskId(String batchTaskId);
 }
