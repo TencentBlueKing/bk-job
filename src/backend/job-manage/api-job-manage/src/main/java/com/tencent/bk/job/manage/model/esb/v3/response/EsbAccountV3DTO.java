6f002b026a0b4d5975131f721e7184b2324bc547
wangyu096 <wangyu096@163.com>
2023-10-24 12:08:03 +0800

    fix: 审计中心回调 Job 获取资源实例 schema 报错 #2541

--- src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/model/esb/v3/response/EsbAccountV3DTO.java
+++ src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/model/esb/v3/response/EsbAccountV3DTO.java
@@ -25,6 +25,7 @@
 package com.tencent.bk.job.manage.model.esb.v3.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.tencent.bk.job.common.esb.model.EsbAppScopeDTO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -32,37 +33,49 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class EsbAccountV3DTO extends EsbAppScopeDTO {
+    @JsonPropertyDescription("Account ID")
     private Long id;
 
+    @JsonPropertyDescription("Account name")
     private String account;
 
+    @JsonPropertyDescription("Account alias")
     private String alias;
 
     // 账号用途（1：系统账号，2：DB账号）
+    @JsonPropertyDescription("Account usage. 1- OS Account, 2 - DB Account")
     private int category;
 
     // 账号类型（1：Linux，2：Windows，9：MySQL，10：Oracle，11：DB2）
+    @JsonPropertyDescription("Account type. 1 - Linux, 2 - Windows, 9 - MySQL, 10 - Oracle, 11 - DB2")
     private int type;
 
     @JsonProperty("db_system_account_id")
+    @JsonPropertyDescription("System account id for db account")
     private Long dbSystemAccountId;
 
+    @JsonPropertyDescription("Account OS")
     private String os;
 
+    @JsonPropertyDescription("Creator")
     private String creator;
 
     @JsonProperty("create_time")
+    @JsonPropertyDescription("Create time")
     private Long createTime;
 
     @JsonProperty("last_modify_user")
+    @JsonPropertyDescription("Last modify user")
     private String lastModifyUser;
 
+    @JsonPropertyDescription("Last modify time")
     @JsonProperty("last_modify_time")
     private Long lastModifyTime;
 
     /**
      * 账号描述
      */
+    @JsonPropertyDescription("Account description")
     @JsonProperty("description")
     private String description;
 }
