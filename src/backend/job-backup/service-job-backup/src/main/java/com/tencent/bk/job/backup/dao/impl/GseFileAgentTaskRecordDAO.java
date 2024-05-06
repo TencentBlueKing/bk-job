c58ea105a65fa09731c6e263751a4506fcb90192
wangyu096 <wangyu096@163.com>
2024-05-06 14:37:33 +0800

    fix: 执行历史归档读取数据重复，导致归档数据写入冲突 #2952

--- src/backend/job-backup/service-job-backup/src/main/java/com/tencent/bk/job/backup/dao/impl/GseFileAgentTaskRecordDAO.java
+++ src/backend/job-backup/service-job-backup/src/main/java/com/tencent/bk/job/backup/dao/impl/GseFileAgentTaskRecordDAO.java
@@ -3,8 +3,13 @@ package com.tencent.bk.job.backup.dao.impl;
 import com.tencent.bk.job.execute.model.tables.GseFileAgentTask;
 import com.tencent.bk.job.execute.model.tables.records.GseFileAgentTaskRecord;
 import org.jooq.DSLContext;
+import org.jooq.OrderField;
 import org.jooq.Table;
 import org.jooq.TableField;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * gse_file_agent_task DAO
@@ -12,6 +17,15 @@ import org.jooq.TableField;
 public class GseFileAgentTaskRecordDAO extends AbstractExecuteRecordDAO<GseFileAgentTaskRecord> {
 
     private static final GseFileAgentTask TABLE = GseFileAgentTask.GSE_FILE_AGENT_TASK;
+    private static final List<OrderField<?>> ORDER_FIELDS = new ArrayList<>();
+
+    static {
+        ORDER_FIELDS.add(GseFileAgentTask.GSE_FILE_AGENT_TASK.STEP_INSTANCE_ID.asc());
+        ORDER_FIELDS.add(GseFileAgentTask.GSE_FILE_AGENT_TASK.EXECUTE_COUNT.asc());
+        ORDER_FIELDS.add(GseFileAgentTask.GSE_FILE_AGENT_TASK.BATCH.asc());
+        ORDER_FIELDS.add(GseFileAgentTask.GSE_FILE_AGENT_TASK.MODE.asc());
+        ORDER_FIELDS.add(GseFileAgentTask.GSE_FILE_AGENT_TASK.HOST_ID.asc());
+    }
 
     public GseFileAgentTaskRecordDAO(DSLContext context) {
         super(context);
@@ -25,5 +39,10 @@ public class GseFileAgentTaskRecordDAO extends AbstractExecuteRecordDAO<GseFileA
     @Override
     public TableField<GseFileAgentTaskRecord, Long> getArchiveIdField() {
         return TABLE.STEP_INSTANCE_ID;
+    }
+
+    @Override
+    protected Collection<? extends OrderField<?>> getListRecordsOrderFields() {
+        return ORDER_FIELDS;
     }
 }
