f8cbf13f21e7cddac6214fe028593f4c1472a5e2
wangyu096 <wangyu096@163.com>
2024-06-27 16:54:38 +0800

    feat: 执行引擎任务调度配额限制 #261

--- src/backend/job-execute/service-job-execute/src/main/resources/lua/add_running_job.lua
+++ src/backend/job-execute/service-job-execute/src/main/resources/lua/add_running_job.lua
@@ -6,7 +6,7 @@ local resource_scope = ARGV[2]
 local app_code = ARGV[3]
 local job_create_time = tonumber(ARGV[4])
 
-local add_result = redis.call('zadd', system_running_job_zset_key, 'NX', job_id, job_create_time)
+local add_result = redis.call('zadd', system_running_job_zset_key, 'NX', job_create_time, job_id)
 if add_result == 1 then
   redis.call('hincrby', resource_scope_job_count_hash_key, resource_scope, 1)
 
