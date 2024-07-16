603830757b944d1fde7c555c2d11d8b8c37aca9d
wangyu096 <wangyu096@163.com>
2024-07-16 14:38:30 +0800

    feat: Job 支持分库分表 #2605

--- src/backend/commons/common-mysql-sharding/src/main/java/com/tencent/bk/job/common/sharding/mysql/config/ShardingDatasourceAutoConfiguration.java
+++ src/backend/commons/common-mysql-sharding/src/main/java/com/tencent/bk/job/common/sharding/mysql/config/ShardingDatasourceAutoConfiguration.java
@@ -24,10 +24,114 @@
 
 package com.tencent.bk.job.common.sharding.mysql.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.shardingsphere.driver.api.ShardingSphereDataSourceFactory;
+import org.apache.shardingsphere.infra.config.mode.ModeConfiguration;
+import org.apache.shardingsphere.infra.config.rule.RuleConfiguration;
+import org.apache.shardingsphere.mode.repository.standalone.StandalonePersistRepositoryConfiguration;
+import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties({ShardingsphereProperties.class})
 public class ShardingDatasourceAutoConfiguration {
+
+    private ShardingsphereProperties shardingsphereProperties;
+
+
+    @Bean("shardingDataSource")
+    public DataSource shardingDataSource(ShardingsphereProperties shardingsphereProperties) throws SQLException {
+        // 指定逻辑 Database 名称
+        String databaseName = "job_execute";
+        // 构建运行模式
+        ModeConfiguration modeConfig = createModeConfiguration();
+        // 构建真实数据源
+        Map<String, DataSource> dataSourceMap = createDataSources();
+        // 构建具体规则
+        List<RuleConfiguration> ruleConfigs = new ArrayList<>();
+        ruleConfigs.add(createShardingRuleConfiguration());
+        return ShardingSphereDataSourceFactory.createDataSource(databaseName, modeConfig,
+            dataSourceMap, ruleConfigs, null);
+    }
+
+    private ModeConfiguration createModeConfiguration() {
+        return new ModeConfiguration("Standalone",
+            new StandalonePersistRepositoryConfiguration("JDBC", new Properties()));
+    }
+
+    private Map<String, DataSource> createDataSources() {
+        Map<String, DataSource> dataSourceMap = new HashMap<>();
+        // 配置第 1 个数据源
+        HikariDataSource dataSource1 = new HikariDataSource();
+        dataSource1.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource1.setJdbcUrl("jdbc:mysql://localhost:3306/ds_1");
+        dataSource1.setUsername("root");
+        dataSource1.setPassword("");
+        dataSourceMap.put("ds_1", dataSource1);
+
+        // 配置第 2 个数据源
+        HikariDataSource dataSource2 = new HikariDataSource();
+        dataSource2.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource2.setJdbcUrl("jdbc:mysql://localhost:3306/ds_2");
+        dataSource2.setUsername("root");
+        dataSource2.setPassword("");
+        dataSourceMap.put("ds_2", dataSource2);
+    }
+
+    private ShardingRuleConfiguration createShardingRuleConfiguration() {
+        ShardingRuleConfiguration result = new ShardingRuleConfiguration();
+        result.getTables().add(getOrderTableRuleConfiguration());
+        result.getTables().add(getOrderItemTableRuleConfiguration());
+        result.getBindingTableGroups().add(new ShardingTableReferenceRuleConfiguration("foo", "t_order, t_order_item"));
+        result.setDefaultDatabaseShardingStrategy(new StandardShardingStrategyConfiguration("user_id", "inline"));
+        result.setDefaultTableShardingStrategy(new StandardShardingStrategyConfiguration("order_id",
+            "standard_test_tbl"));
+        Properties props = new Properties();
+        props.setProperty("algorithm-expression", "demo_ds_${user_id % 2}");
+        result.getShardingAlgorithms().put("inline", new AlgorithmConfiguration("INLINE", props));
+        result.getShardingAlgorithms().put("standard_test_tbl", new AlgorithmConfiguration("STANDARD_TEST_TBL",
+            new Properties()));
+        result.getKeyGenerators().put("snowflake", new AlgorithmConfiguration("SNOWFLAKE", new Properties()));
+        result.getAuditors().put("sharding_key_required_auditor", new AlgorithmConfiguration("DML_SHARDING_CONDITIONS"
+            , new Properties()));
+        return result;
+    }
+
+    private ShardingTableRuleConfiguration getOrderTableRuleConfiguration() {
+        ShardingTableRuleConfiguration result = new ShardingTableRuleConfiguration("t_order", "demo_ds_${0..1}" +
+            ".t_order_${[0, 1]}");
+        result.setKeyGenerateStrategy(new KeyGenerateStrategyConfiguration("order_id", "snowflake"));
+        result.setAuditStrategy(new ShardingAuditStrategyConfiguration(Collections.singleton(
+            "sharding_key_required_auditor"), true));
+        return result;
+    }
+
+    private ShardingTableRuleConfiguration getOrderItemTableRuleConfiguration() {
+        ShardingTableRuleConfiguration result = new ShardingTableRuleConfiguration("t_order_item", "demo_ds_${0..1}" +
+            ".t_order_item_${[0, 1]}");
+        result.setKeyGenerateStrategy(new KeyGenerateStrategyConfiguration("order_item_id", "snowflake"));
+        return result;
+    }
+
+    private Map<String, DataSource> createDataSourceMap() {
+        Map<String, DataSource> result = new HashMap<>();
+        result.put("demo_ds_0", DataSourceUtil.createDataSource("demo_ds_0"));
+        result.put("demo_ds_1", DataSourceUtil.createDataSource("demo_ds_1"));
+        return result;
+    }
+
+    private BroadcastRuleConfiguration createBroadcastRuleConfiguration() {
+        return new BroadcastRuleConfiguration(Collections.singletonList("t_address"));
+        ;
+    }
 }
--- src/backend/commons/common-mysql-sharding/src/main/java/com/tencent/bk/job/common/sharding/mysql/config/ShardingsphereProperties.java
+++ src/backend/commons/common-mysql-sharding/src/main/java/com/tencent/bk/job/common/sharding/mysql/config/ShardingsphereProperties.java
@@ -36,17 +36,35 @@ import java.util.Map;
 @Setter
 @ToString
 public class ShardingsphereProperties {
+    /**
+     * 数据源集群。 key:集群名称；value:集群配置
+     */
     private Map<String, DataSourceCluster> dataSourceClusters;
+    /**
+     * 分片规则
+     */
     private ShardingRule shardingRule;
 
+    /**
+     * 数据源集群
+     */
     @Getter
     @Setter
     @ToString
     private static class DataSourceCluster {
+        /**
+         * 数据源完整类名
+         */
         private String dataSourceClassName;
+        /**
+         * 数据库驱动类名，以数据库连接池自身配置为准
+         */
         private String driverClassName;
         private String username;
         private String password;
+        /**
+         * 数据源配置。 key:数据源名称;value: 数据源配置详情。
+         */
         private Map<String, DataSource> dataSources;
     }
 
@@ -54,17 +72,48 @@ public class ShardingsphereProperties {
     @Setter
     @ToString
     private static class DataSource {
+        /**
+         * 数据源名称
+         */
         private String name;
+        /**
+         * 数据库 URL 连接，以数据库连接池自身配置为准
+         */
         private String jdbcUrl;
     }
 
 
+    /**
+     * 数据分片规则配置
+     */
     @Getter
     @Setter
     @ToString
     private static class ShardingRule {
+        /**
+         * 表对应的分片规则。 key: 表名称;value: 分片规则
+         */
         private Map<String, TableShardingRule> tables;
+        /**
+         * 分片算法定义
+         */
         private Map<String, ShardingAlgorithm> shardingAlgorithms;
+        /**
+         * 默认数据库分片策略
+         */
+        private ShardingStrategy defaultDatabaseStrategy;
+        /**
+         * 默认表分片策略
+         */
+        private ShardingStrategy defaultTableStrategy;
+        /**
+         * 默认分片列名称
+         */
+        private String defaultShardingColumn;
+        /**
+         * 默认的分布式序列策略
+         */
+        private String defaultKeyGenerateStrategy;
     }
 
 
@@ -72,34 +121,71 @@ public class ShardingsphereProperties {
     @Setter
     @ToString
     private static class TableShardingRule {
+        /**
+         * 表所在的 db 数据源集群
+         */
         private String dataSourceCluster;
+        /**
+         * 由数据源名 + 表名组成
+         */
         private String actualDataNodes;
-        private TableStrategy tableStrategy;
+        /**
+         * 分库策略
+         */
+        private ShardingStrategy databaseStrategy;
+        /**
+         * 分表策略，同分库策略
+         */
+        private ShardingStrategy tableStrategy;
+        /**
+         * 分布式序列策略
+         */
         private KeyGenerateStrategy keyGenerateStrategy;
     }
 
+    /**
+     * 分片计算策略
+     */
     @Getter
     @Setter
     @ToString
-    private static class TableStrategy {
+    private static class ShardingStrategy {
         private String type;
         private String shardingColumn;
         private String shardingAlgorithmName;
     }
 
+    /**
+     * 分布式序列策略
+     */
     @Getter
     @Setter
     @ToString
     private static class KeyGenerateStrategy {
+        /**
+         * 自增列名称，缺省表示不使用自增主键生成器
+         */
         private String column;
+        /**
+         * 分布式序列算法名称
+         */
         private String keyGeneratorName;
     }
 
+    /**
+     * 分片算法配置
+     */
     @Getter
     @Setter
     @ToString
     private static class ShardingAlgorithm {
+        /**
+         * 分片算法类型
+         */
         private String type;
+        /**
+         * 分片算法属性配置
+         */
         private Map<String, String> props;
     }
 
