4a27be55870fc8e0eed89af679c71d8c6ca4245e
jsonwan <jsonwan@tencent.com>
2024-10-10 22:37:45 +0800

    fix: 更新过程中服务间调用失败导致无损更新失效 #3242
    1.调整GracefulShutdown等待时间为40s，避免边界误差导致负载均衡缓存未刷新。

--- src/backend/commons/common-k8s/src/main/java/com/tencent/bk/job/common/k8s/availability/JobApplicationAvailabilityBean.java
+++ src/backend/commons/common-k8s/src/main/java/com/tencent/bk/job/common/k8s/availability/JobApplicationAvailabilityBean.java
@@ -13,7 +13,7 @@ public class JobApplicationAvailabilityBean extends ApplicationAvailabilityBean 
         super.onApplicationEvent(event);
         if (ReadinessState.REFUSING_TRAFFIC == event.getState()) {
             // SpringCloud负载均衡缓存默认为35s，等待调用方缓存刷新后再真正关闭Spring容器
-            int waitSeconds = 60;
+            int waitSeconds = 40;
             while (waitSeconds > 0) {
                 ThreadUtils.sleep(1000);
                 log.info("wait for GracefulShutdown, {}s left", waitSeconds--);
