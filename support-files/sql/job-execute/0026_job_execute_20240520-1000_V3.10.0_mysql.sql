27a087c9ca984e746202c4b27de42d784c826810
wangyu096 <wangyu096@163.com>
2024-10-16 15:24:26 +0800

    feat: Job 支持分库分表 #2605

--- support-files/sql/job-execute/0026_job_execute_20240520-1000_V3.10.0_mysql.sql
+++ support-files/sql/job-execute/0026_job_execute_20240520-1000_V3.10.0_mysql.sql
@@ -2,6 +2,7 @@ USE job_execute;
 
 SET NAMES utf8mb4;
 
+-- 更新 schema
 DROP PROCEDURE IF EXISTS job_schema_update;
 
 DELIMITER <JOB_UBF>
@@ -45,18 +46,19 @@ BEGIN
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
   
   
-  CREATE TABLE IF NOT EXISTS `task_instance_host_app` (
-    `task_instance_id` bigint(20) NOT NULL DEFAULT '0',
-    `host_id` bigint(20) NOT NULL DEFAULT '0',
-    `ip` varchar(15) DEFAULT NULL,
-    `ipv6` varchar(46) DEFAULT NULL,
-    `app_id` bigint(20) NOT NULL,
-    `row_create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `row_update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`task_instance_id`,`host_id`) USING BTREE,
-    KEY `idx_ip` (`ip`) USING BTREE,
-    KEY `idx_ipv6` (`ipv6`) USING BTREE
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  -- CREATE TABLE IF NOT EXISTS `task_instance_host_app` (
+  --   `task_instance_id` bigint(20) NOT NULL DEFAULT '0',
+  --   `host_id` bigint(20) NOT NULL DEFAULT '0',
+  --   `ip` varchar(15) DEFAULT NULL,
+  --   `ipv6` varchar(46) DEFAULT NULL,
+  --   `app_id` bigint(20) NOT NULL,
+  --   `row_create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  --   `row_update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  --   PRIMARY KEY (`task_instance_id`,`host_id`) USING BTREE,
+  --   KEY `idx_ip` (`ip`) USING BTREE,
+  --   KEY `idx_ipv6` (`ipv6`) USING BTREE
+  -- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
   -- task_instance 
   IF NOT EXISTS(SELECT 1
@@ -72,6 +74,22 @@ BEGIN
                   AND TABLE_NAME = 'task_instance'
                   AND INDEX_NAME = 'idx_create_time') THEN
       ALTER TABLE task_instance DROP INDEX `idx_create_time`;
+  END IF;
+
+  -- task_instance_host
+  IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'task_instance_host'
+                    AND COLUMN_NAME = 'app_id') THEN
+    ALTER TABLE task_instance_host ADD COLUMN `app_id` bigint(20) NOT NULL DEFAULT '0';
+  END IF;
+  IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'task_instance_host'
+                    AND COLUMN_NAME = 'job_create_time') THEN
+    ALTER TABLE task_instance_host ADD COLUMN `job_create_time` bigint(20) NOT NULL DEFAULT '0';
   END IF;
 
 
@@ -316,3 +334,183 @@ COMMIT;
 CALL job_schema_update();
 
 DROP PROCEDURE IF EXISTS job_schema_update;
+
+
+
+-- 更新 task_instance_id 列
+DROP PROCEDURE IF EXISTS job_add_task_instance_id;
+
+DELIMITER <JOB_UBF>
+
+CREATE PROCEDURE job_add_task_instance_id()
+BEGIN
+
+  DECLARE minId BIGINT;
+  DECLARE maxId BIGINT;
+  DECLARE fromId BIGINT;
+  DECLARE endId BIGINT;
+
+  SET AUTOCOMMIT = 0;
+
+  SELECT MIN(id), MAX(id) INTO minId, maxId FROM step_instance;
+
+  SET fromId = minId;
+
+  WHILE fromId <= maxId DO
+    SET endId = fromId + 999;
+
+    UPDATE file_source_task_log t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE gse_file_agent_task t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE gse_file_execute_obj_task t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE gse_script_agent_task t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE gse_script_execute_obj_task t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE gse_task t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE step_instance_confirm t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE step_instance_script t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE step_instance_file t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE step_instance_rolling_task t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    UPDATE step_instance_variable t1
+    INNER JOIN (
+      SELECT id, task_instance_id 
+      FROM step_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.step_instance_id = tmp.id 
+    SET t1.task_instance_id = tmp.task_instance_id
+    WHERE t1.task_instance_id = 0;
+
+    COMMIT;
+
+    SET fromId = endId + 1;
+  END WHILE;
+  
+END <JOB_UBF>
+DELIMITER ;
+COMMIT;
+
+CALL job_add_task_instance_id();
+DROP PROCEDURE IF EXISTS job_add_task_instance_id;
+
+
+-- 更新 task_instance_host 表字段数据
+DROP PROCEDURE IF EXISTS job_update_task_instance_host_data;
+
+DELIMITER <JOB_UBF>
+
+CREATE PROCEDURE job_update_task_instance_host_data()
+BEGIN
+
+  DECLARE minId BIGINT;
+  DECLARE maxId BIGINT;
+  DECLARE fromId BIGINT;
+  DECLARE endId BIGINT;
+
+  SET AUTOCOMMIT = 0;
+
+  SELECT MIN(id), MAX(id) INTO minId, maxId FROM task_instance;
+
+  SET fromId = minId;
+
+  WHILE fromId <= maxId DO
+    SET endId = fromId + 999;
+
+    UPDATE task_instance_host t1
+    INNER JOIN (
+      SELECT id,app_id,create_time 
+      FROM task_instance 
+      WHERE id BETWEEN fromId AND endId)
+    AS tmp ON t1.task_instance_id = tmp.id 
+    SET t1.app_id = tmp.app_id, t1.job_create_time = tmp.create_time
+    WHERE t1.app_id = 0;
+
+    COMMIT;
+
+    SET fromId = endId + 1;
+  END WHILE;
+  
+END <JOB_UBF>
+DELIMITER ;
+COMMIT;
+
+CALL job_update_task_instance_host_data();
+DROP PROCEDURE IF EXISTS job_update_task_instance_host_data;
+
