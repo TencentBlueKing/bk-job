778b355c5ec527708ec2c382d4c7bd1f99ef5560
jsonwan <jsonwan@tencent.com>
2024-10-30 18:03:20 +0800

    feature: 执行类操作支持按业务拓扑节点申请权限与鉴权 #3213
    1.修复初始化时序导致的部分接口调用指标丢失问题。

--- src/backend/commons/common/src/main/java/com/tencent/bk/job/common/util/http/WatchableHttpHelper.java
+++ src/backend/commons/common/src/main/java/com/tencent/bk/job/common/util/http/WatchableHttpHelper.java
@@ -36,6 +36,7 @@ import org.apache.http.client.methods.HttpRequestBase;
 import java.util.AbstractList;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * 对Http请求状态码与异常情况进行统计便于监控
@@ -43,11 +44,11 @@ import java.util.function.Function;
 public class WatchableHttpHelper implements HttpHelper {
 
     private final HttpHelper httpHelper;
-    private final MeterRegistry meterRegistry;
+    private final Supplier<MeterRegistry> meterRegistrySupplier;
 
-    public WatchableHttpHelper(HttpHelper httpHelper, MeterRegistry meterRegistry) {
+    public WatchableHttpHelper(HttpHelper httpHelper, Supplier<MeterRegistry> meterRegistrySupplier) {
         this.httpHelper = httpHelper;
-        this.meterRegistry = meterRegistry;
+        this.meterRegistrySupplier = meterRegistrySupplier;
     }
 
     @Override
@@ -82,6 +83,7 @@ public class WatchableHttpHelper implements HttpHelper {
             AbstractList<Tag> httpMetricTags = HttpMetricUtil.getCurrentMetricTags();
             httpMetricTags.add(Tag.of("http_status",
                 StringUtils.isNotEmpty(httpStatusTagValue) ? httpStatusTagValue : "UNKNOWN"));
+            MeterRegistry meterRegistry = meterRegistrySupplier.get();
             if (meterRegistry != null && StringUtils.isNotBlank(httpMetricName)) {
                 meterRegistry.timer(httpMetricName, httpMetricTags)
                     .record(end - start, TimeUnit.NANOSECONDS);
