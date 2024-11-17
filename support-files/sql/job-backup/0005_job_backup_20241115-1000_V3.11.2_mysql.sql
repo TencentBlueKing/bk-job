6d9a6c24b2de9b765903cb96c120ed7e754d8bdf
wangyu096 <wangyu096@163.com>
2024-11-17 18:07:35 +0800

    perf: 分库分表改造-任务表以及子表加入 task_instance_id 字段 #2991

---
+++ support-files/sql/job-backup/0005_job_backup_20241115-1000_V3.11.2_mysql.sql
@@ -0,0 +1,22 @@
+USE `job_backup`;
+
+SET NAMES utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `archive_task` (
+  `row_create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `row_update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `task_type` TINYINT(2),
+  `data_node` VARCHAR(128) NOT NULL,
+  `day` INT(8),
+  `hour` TINYINT(2),
+  `from_timestamp` BIGINT(20) NOT NULL,
+  `to_timestamp` BIGINT(20) NOT NULL,
+  `process` VARCHAR(256),
+  `status` TINYINT(2) NOT NULL DEFAULT '0',
+  `create_time` BIGINT(20) NOT NULL,
+  `last_update_time` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `uk_type_node_day_hour` (`task_type`,`data_node`,`day`,`hour`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
