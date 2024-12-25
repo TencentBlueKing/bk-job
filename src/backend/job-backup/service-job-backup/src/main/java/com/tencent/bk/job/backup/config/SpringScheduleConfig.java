91629175331421a5020598f9d9d9fdd983752787
wangyu096 <wangyu096@163.com>
2024-12-25 15:01:22 +0800

    fix: 使用 spring 定时任务，线程池配置不合理导致定时任务阻塞 #3355

--- src/backend/job-backup/service-job-backup/src/main/java/com/tencent/bk/job/backup/config/SpringScheduleConfig.java
+++ src/backend/job-backup/service-job-backup/src/main/java/com/tencent/bk/job/backup/config/SpringScheduleConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 @Configuration
 @Slf4j
@@ -13,7 +13,8 @@ public class SpringScheduleConfig implements SchedulingConfigurer {
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         log.info("Configure spring cron task scheduler");
         //设定一个长度5的定时任务线程池
-        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(5));
+        taskRegistrar.setScheduler(new ScheduledThreadPoolExecutor(5, (r, executor) -> log.error(
+            "ScheduledThreadPoolExecutor rejected a runnable")));
     }
 
 }
