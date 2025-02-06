affa7239d035b0a4635080f5ab38f9d9cc81197e
lorryyzwu <lorryyzwu@tencent.com>
2025-02-06 10:46:03 +0800

    perf: 优化插入MySQL时脚本参数过长的报错信息，使用validation校验脚本长度 #3374

--- src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/model/web/vo/task/TaskScriptStepVO.java
+++ src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/model/web/vo/task/TaskScriptStepVO.java
@@ -26,8 +26,10 @@ package com.tencent.bk.job.manage.model.web.vo.task;
 
 import com.tencent.bk.job.common.constant.ErrorCode;
 import com.tencent.bk.job.common.constant.JobConstants;
+import com.tencent.bk.job.common.constant.MySQLTextDataType;
 import com.tencent.bk.job.common.exception.InvalidParamException;
 import com.tencent.bk.job.common.model.vo.TaskTargetVO;
+import com.tencent.bk.job.common.validation.NotExceedMySQLTextFieldLength;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -50,12 +52,22 @@ public class TaskScriptStepVO {
     private Long scriptVersionId;
 
     @ApiModelProperty("脚本内容")
+    @NotExceedMySQLTextFieldLength(
+        fieldName = "content",
+        fieldType = MySQLTextDataType.MEDIUMTEXT,
+        base64 = true
+    )
     private String content;
 
     @ApiModelProperty("脚本语言")
     private Integer scriptLanguage;
 
     @ApiModelProperty("脚本参数")
+    @NotExceedMySQLTextFieldLength(
+        fieldName = "scriptParam",
+        fieldType = MySQLTextDataType.TEXT,
+        base64 = false
+    )
     private String scriptParam;
 
     @ApiModelProperty("脚本超时时间")
