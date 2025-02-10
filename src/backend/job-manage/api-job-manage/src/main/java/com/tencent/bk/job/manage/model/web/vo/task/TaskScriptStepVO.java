d04c5726531efcb0ad560f50e8ab3cccfe91b64e
jsonwan <jsonwan@tencent.com>
2025-02-10 14:46:45 +0800

    feature: 支持指定脚本解释器运行脚本 #3321
    优化Web接口后缀校验报错信息。

--- src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/model/web/vo/task/TaskScriptStepVO.java
+++ src/backend/job-manage/api-job-manage/src/main/java/com/tencent/bk/job/manage/model/web/vo/task/TaskScriptStepVO.java
@@ -61,7 +61,8 @@ public class TaskScriptStepVO {
     private String scriptParam;
 
     @ApiModelProperty("自定义Windows解释器路径")
-    @EndWith(fieldName = "windowsInterpreter", value = ".exe")
+    @EndWith(fieldName = "windowsInterpreter", value = ".exe",
+        message = "validation.constraints.WinInterpreterInvalidSuffix.message")
     @MaxLength(value = 260,
         message = "{validation.constraints.WindowsInterpreterExceedMaxLength.message}")
     private String windowsInterpreter;
