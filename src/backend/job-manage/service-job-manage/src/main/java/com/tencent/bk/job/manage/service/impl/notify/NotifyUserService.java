45fdaa232c11e664d8e377e2d1a4a29835d9d09e
lorryyzwu <lorryyzwu@tencent.com>
2025-04-02 19:40:37 +0800

    feat: Job 支持多租户 #3369
    对接cmsi

--- src/backend/job-manage/service-job-manage/src/main/java/com/tencent/bk/job/manage/service/impl/notify/NotifyUserService.java
+++ src/backend/job-manage/service-job-manage/src/main/java/com/tencent/bk/job/manage/service/impl/notify/NotifyUserService.java
@@ -35,7 +35,6 @@ import com.tencent.bk.job.manage.model.web.vo.notify.UserVO;
 import com.tencent.bk.job.manage.service.UserCacheService;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
