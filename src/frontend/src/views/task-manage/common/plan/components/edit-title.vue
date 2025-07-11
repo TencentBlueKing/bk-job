1324feede43b6dfddee6195daaf76f5b885e268b
jsonwan <jsonwan@tencent.com>
2025-07-11 15:35:20 +0800

    docs: Tencent开源License更新 #3634

--- src/frontend/src/views/task-manage/common/plan/components/edit-title.vue
+++ src/frontend/src/views/task-manage/common/plan/components/edit-title.vue
@@ -1,7 +1,7 @@
 <!--
  * Tencent is pleased to support the open source community by making BK-JOB蓝鲸智云作业平台 available.
  *
- * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ * Copyright (C) 2021 Tencent.  All rights reserved.
  *
  * BK-JOB蓝鲸智云作业平台 is licensed under the MIT License.
  *
