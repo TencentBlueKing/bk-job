71bdd09829cc7d1aeeb523ac246b7b3ad0a3ab54
liuliaozhong <liuliaozhong@canway.net>
2025-10-23 10:28:57 +0800

    feature: 容器化-升级过程中的SQL更新支持增量更新 #1646
    1.startup.sh脚本中记录成功执行的sql脚本

--- support-files/kubernetes/images/migration/startup.sh
+++ support-files/kubernetes/images/migration/startup.sh
@@ -7,25 +7,53 @@ find .
 echo "===========ENV========="
 env
 echo "===========EXEC========"
-MYSQL_CMD="mysql $BK_JOB_TLS_OPTIONS $BK_JOB_MYSQL_EXTRA_OPTIONS"
-echo "MYSQL_CMD=$MYSQL_CMD"
-
+MYSQL_BASE_CMD="mysql $BK_JOB_TLS_OPTIONS $BK_JOB_MYSQL_EXTRA_OPTIONS"
+echo "MYSQL_BASE_CMD=MYSQL_BASE_CMD"
 echo "sleep ${BK_JOB_SLEEP_SECONDS_BEFORE_MIGRATION}s before migration"
 sleep ${BK_JOB_SLEEP_SECONDS_BEFORE_MIGRATION}
 echo "sleep end"
+$MYSQL_BASE_CMD --version
 
-$MYSQL_CMD --version
+MYSQL_CMD="$MYSQL_BASE_CMD -h$BK_JOB_MYSQL_HOST -P$BK_JOB_MYSQL_PORT -u$BK_JOB_MYSQL_ADMIN_USERNAME -p$BK_JOB_MYSQL_ADMIN_PASSWORD"
 
 # 记录退出码
 exitCode=0
 
 function checkMysql(){
-  c=$($MYSQL_CMD -h $BK_JOB_MYSQL_HOST -P $BK_JOB_MYSQL_PORT -u$BK_JOB_MYSQL_ADMIN_USERNAME -p$BK_JOB_MYSQL_ADMIN_PASSWORD -e "select 1"|grep 1|wc -l)
+  c=$($MYSQL_CMD -e "select 1"|grep 1|wc -l)
   echo "c=$c"
   if [[ "$c" == "2" ]];then
     return 0
   fi
   return 1
+}
+
+# 在job_manage库中创建migration表，用于升级时记录成功执行的sql脚本
+function initMigrationHistoryTable() {
+  $MYSQL_CMD -e "
+  SET NAMES utf8mb4;
+  CREATE DATABASE IF NOT EXISTS job_manage DEFAULT CHARACTER SET utf8mb4;
+  USE job_manage;
+  CREATE TABLE IF NOT EXISTS db_migration_history (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    db_name VARCHAR(64) NOT NULL,
+    script_name VARCHAR(128) NOT NULL,
+    executed_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY uk_migration_db_name_script_name (db_name, script_name)
+  );"
+}
+
+# 从sql脚本的路径中解析到库名称和脚本名称
+function parseNameFromPath() {
+    local filePath="$1"
+    local fileName
+    # 命名规范：{序号}_{db名称}_{创建时间}-{脚本序号}_{版本号}_{db类型}.sql
+    fileName=$(basename "$filePath")
+    # 截取 - 左边部分，去掉开头的序号_，去掉最后一个下划线及之后的内容
+    local left_part="${fileName%%-*}"
+    local tmp="${left_part#*_}"
+    local dbName="${tmp%_*}"
+    echo "$dbName|$fileName"
 }
 
 function migrateMySQL(){
@@ -39,10 +67,27 @@ function migrateMySQL(){
     isMysqlOK=$?
   done
 
+  initMigrationHistoryTable
+
   for sql in "${ALL_SQL[@]}"; do
+    nameInfo=$(parseNameFromPath "$sql")
+    dbName=$(echo "$nameInfo" | cut -d'|' -f1)
+    scriptName=$(echo "$nameInfo" | cut -d'|' -f2)
+
+    # 如果执行过跳过
+    executed=$($MYSQL_CMD -e "SELECT 1 FROM job_manage.db_migration_history WHERE db_name='$dbName' AND script_name='$scriptName' LIMIT 1;")
+    if [[ "$executed" == "1" ]]; then
+      echo "Skip $sql, already executed."
+      continue
+    fi
+
     echo "migrate $sql"
-    $MYSQL_CMD -h $BK_JOB_MYSQL_HOST -P $BK_JOB_MYSQL_PORT -u$BK_JOB_MYSQL_ADMIN_USERNAME -p$BK_JOB_MYSQL_ADMIN_PASSWORD < $sql
-    echo "migrate $sql done"
+    if $MYSQL_EXEC_CMD < $sql; then
+        $MYSQL_EXEC_CMD -e "INSERT INTO job_manage.db_migration_history(db_name, script_name) VALUES('$dbName','$fileName');"
+        echo "Migrate $sql success"
+    else
+        echo "Migrate $sql failed"
+    fi
   done
 }
 
