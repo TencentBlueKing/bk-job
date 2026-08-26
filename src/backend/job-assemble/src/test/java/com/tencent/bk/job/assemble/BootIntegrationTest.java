633c266ad0a7ac0776b5138bec05af6641941a2e
lorryyzwu <lorryyzwu@tencent.com>
2026-08-26 16:42:11 +0800

    perf: 文件源使用优化 #4456
    修复checkstyle问题

--- src/backend/job-assemble/src/test/java/com/tencent/bk/job/assemble/BootIntegrationTest.java
+++ src/backend/job-assemble/src/test/java/com/tencent/bk/job/assemble/BootIntegrationTest.java
@@ -26,8 +26,6 @@ package com.tencent.bk.job.assemble;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
